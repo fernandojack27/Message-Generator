cf8d95bf1e04eaefb2ba02a6152fc57893730798
fernandojack27 <160216055+fernandojack27@users.noreply.github.com>
2024-02-24 14:13:33 +0200

    user-input.py
     This is a program that asks the user for their name, age, and location and then prints out a personalized message.

---
+++ user-input.py
@@ -0,0 +1,4 @@
+name = input("Jacobo: ")
+age = input("27: ")
+location = input("Mozambique: ")
+print("Hello My name is Jacobo, I am 27 years old and I live in Mozambique.".format(name, age, location))
